7065e132e4f57ce6da1530cdb168d084da27c50b
pmanish10 <80895406+pmanish10@users.noreply.github.com>
2021-10-17 21:07:29 +0530

    Add files via upload

---
+++ nevnav.js
@@ -0,0 +1,20 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "70%";
+    // document.getElementById("flipkart-navbar").style.width = "50%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0)";
+}
+
+
+
+<li class="upper-links"><a class="links" href="https://sewacity.com/">Home</a></li>
+<li class="upper-links"><a class="links" href="#">Grocery</a></li>
+<li class="upper-links"><a class="links" href="#">Electronics</a></li>
+<li class="upper-links"><a class="links" href="#">Footwear</a></li>
+<li class="upper-links"><a class="links" href="#">Cosmetics</a></li>
+<li class="upper-links"><a class="links" href="#">Stationary</a></li>
+<li class="upper-links"><a class="links" href="#">Super Deals</a></li
